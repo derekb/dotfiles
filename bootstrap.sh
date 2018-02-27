ecd3734f1db3ae117fe55b73e022033c02f2a66e
derekb <derek.burkgren@gmail.com>
2018-02-26 17:24:27 -0800

    Add bootstrapper

---
+++ bootstrap.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Create projects directory
+mkdir ~/projects
+
+# Create .extra file
+touch ~/.extra
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function copyDotfiles() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE" \
+		-avh --no-perms . ~;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	copyDotfiles;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		copyDotfiles;
+	fi;
+fi;
+unset copyDotfiles;
